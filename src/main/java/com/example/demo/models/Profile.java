f431b6958ae8abcfd1ec36c0030fccd1b9249842
Bryan <31749742+Geek-Raven@users.noreply.github.com>
2018-07-12 09:51:33 -0700

    profile.java data 7/12

--- src/main/java/com/example/demo/models/Profile.java
+++ src/main/java/com/example/demo/models/Profile.java
@@ -14,6 +14,9 @@ public class Profile {
 	private Integer id;
 	private String firstName;
 	private String lastName;
+	private String email;
+	private String password;
+	private String username;
 	public String getFirstName() {
 		return firstName;
 	}
@@ -25,20 +28,30 @@ public class Profile {
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
-	}
-	private String user;
-	
+	}	
 	public Integer getId() {
 		return id;
 	}
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	public String getUser() {
-		return user;
+	public String getUsername() {
+		return username;
 	}
-	public void setUser(String user) {
-		this.user = user;
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 }
