43c0d1fbb0d01426d080e20deb829ca14e5cc6dc
Vullcan247 <Vullcan247@yahoo.com>
2018-07-16 11:13:05 -0700

    Ryan-Photos7/16

--- src/main/java/com/example/demo/models/Calendar.java
+++ src/main/java/com/example/demo/models/Calendar.java
@@ -11,7 +11,40 @@ import javax.persistence.Table;
 public class Calendar {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer id;
-
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String start;
+    private String end;
+    private String title;
+    private String color;
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getStart() {
+        return start;
+    }
+    public void setStart(String start) {
+        this.start = start;
+    }
+    public String getEnd() {
+        return end;
+    }
+    public void setEnd(String end) {
+        this.end = end;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
--- src/main/java/com/example/demo/models/Photos.java
+++ src/main/java/com/example/demo/models/Photos.java
@@ -1,16 +1,69 @@
 package com.example.demo.models;
+import java.util.ArrayList;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+ 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-@Entity
-@Table (name="photos")
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Service
+//@Entity
+//@Table (name="photos")
 public class Photos {
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer id;
+	
+//	@Id
+//	@GeneratedValue(strategy=GenerationType.AUTO)
+//	private Integer id;
+//	
 
+	private final Path rootLocation = Paths.get("upload-dir");
+ 
+	public void store(MultipartFile file) {
+		try {
+			Files.copy(file.getInputStream(), this.rootLocation.resolve(file.getOriginalFilename()));
+		} catch (Exception e) {
+			throw new RuntimeException("FAIL!");
+		}
+	}
+
+	public Resource loadFile(String filename) {
+		try {
+			Path file = rootLocation.resolve(filename);
+			Resource resource = new UrlResource(file.toUri());
+			if (resource.exists() || resource.isReadable()) {
+				return resource;
+			} else {
+				throw new RuntimeException("FAIL!");
+			}
+		} catch (MalformedURLException e) {
+			throw new RuntimeException("FAIL!");
+		}
+	}
+ 
+	public void deleteAll() {
+		FileSystemUtils.deleteRecursively(rootLocation.toFile());
+	}
+ 
+	public void init() {
+		try {
+			Files.createDirectory(rootLocation);
+		} catch (IOException e) {
+			throw new RuntimeException("Could not initialize storage!");
+		}
+	}
 }
