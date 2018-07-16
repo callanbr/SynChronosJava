9214d32fc966fceef2d9c3029cee865f503c791c
Bryan <31749742+Geek-Raven@users.noreply.github.com>
2018-07-16 16:11:20 -0700

    profile and groups 7/16

--- src/main/java/com/example/demo/models/Chat.java
+++ src/main/java/com/example/demo/models/Chat.java
@@ -8,52 +8,54 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import com.example.demo.models.Profile;
 
 @Entity
 @Table (name="chat")
-public class Chat {
-	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+public class Chat  {
+    @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+ 
     private Integer id;
-    private String user;
-    private String message;
-    public Timestamp timestamp;
-    
-    
-    public String getTimestamp() {
-	     //Date object
-	     Date date= new Date();
-	     
-	     String time = date.toString();
-	     return new String(time);
-	     
-//	        //getTime() returns current time in milliseconds
-//	     long time = date.getTime();
-//	        //Passed the milliseconds to constructor of TimeStamp class 
-//	     Timestamp ts = new Timestamp(time);
-////	     System.out.println("Current Time Stamp: "+ts);
-////	   this.timestamp = ts;
-//		return timestamp = ts;
-	}
-	public void setTimestamp(Timestamp timestamp) {
+   public String username;
+   private String message;
+   public Timestamp timestamp;
+   
+   
+   public String getTimestamp() {
+         //Date object
+         Date date= new Date();
+        
+         String time = date.toString();
+         return new String(time);
+        
+//            //getTime() returns current time in milliseconds
+//         long time = date.getTime();
+//            //Passed the milliseconds to constructor of TimeStamp class 
+//         Timestamp ts = new Timestamp(time);
+////         System.out.println("Current Time Stamp: "+ts);
+////       this.timestamp = ts;
+//        return timestamp = ts;
     }
-	public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public String getUser() {
-        return user;
-    }
-    public void setUser(String user) {
-        this.user = user;
-    }
-    public String getMessage() {
-        return message;
-    }
-    public void setMessage(String message) {
-        this.message = message;
-    }
+    public void setTimestamp(Timestamp timestamp) {
+   }
+    public Integer getId() {
+       return id;
+   }
+   public void setId(Integer id) {
+       this.id = id;
+   }
+   public String getUsername() {
+       return username = Profile.username;
+   }
+   public void setUsername(String username) {
+       this.username = username;
+   }
+   public String getMessage() {
+       return message;
+   }
+   public void setMessage(String message) {
+       this.message = message;
+   }
 
 }
