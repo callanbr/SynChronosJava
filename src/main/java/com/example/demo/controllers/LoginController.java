af95cb9130292593c7eefa51feac49af776f387f
Megreb812 <39529989+Megreb812@users.noreply.github.com>
2018-07-19 20:35:15 -0700

    New login backend

--- src/main/java/com/example/demo/controllers/LoginController.java
+++ src/main/java/com/example/demo/controllers/LoginController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controllers;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,23 +16,37 @@ import io.jsonwebtoken.impl.crypto.MacProvider;
 import java.security.Key;
 
 import com.example.demo.models.Login;
+import com.example.demo.models.Profile;
+import com.example.demo.models.ProfileRepository;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/login")
 public class LoginController {
 	
-
-	@PostMapping()
+@Autowired
+ ProfileRepository profileRepository; 
+	
+ @PostMapping()
 	@ResponseStatus(HttpStatus.OK)
 	public String createLogin(@RequestBody Login login) {
 		Key key = MacProvider.generateKey();
-		
-//	come back
+//		Optional<profile> is my type like int, string, etc. P is my Name. The rest is my location
+		Optional<Profile> p = profileRepository.findFirstByEmail(login.getEmail()); 
+		Profile profile= null;
+		if (p.isPresent() == false) {
+			profile= new Profile(); 
+			profile.setEmail(login.getEmail());
+			profile= profileRepository.save(profile);
+			profileRepository.flush();
+		} else { profile= p.get();
+			
+		}
+
 
 		String compactJws = Jwts.builder()
-		  .setSubject("12345")
-		  .claim("name",  login.getName())
+		  .setSubject(profile.getId().toString())
+		  .claim("email",  login.getEmail())
 		  .signWith(SignatureAlgorithm.HS512, key)
 		  .compact();
     return compactJws;
--- src/main/java/com/example/demo/models/ProfileRepository.java
+++ src/main/java/com/example/demo/models/ProfileRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.models;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,5 +10,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin(origins="http://localhost:4200")
 public interface ProfileRepository extends JpaRepository<Profile, Integer>{
 	
+		Optional<Profile> findFirstByEmail(String email);
 		
 }
