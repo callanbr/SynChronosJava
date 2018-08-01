a852c46ae64c1e02d0a405d1178c8c8449150b58
Megreb812 <39529989+Megreb812@users.noreply.github.com>
2018-07-31 19:53:05 -0700

    Commit

--- src/main/java/com/example/demo/controllers/CalendarController.java
+++ src/main/java/com/example/demo/controllers/CalendarController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.models.Calendar;
 import com.example.demo.models.CalendarRepository;
 import com.example.demo.models.Chat;
+import com.example.demo.models.Profile;
+import com.example.demo.models.ProfileRepository;
 
 @RestController
 @RequestMapping("/calendar")
@@ -22,17 +26,22 @@ public class CalendarController {
 	
 	@Autowired
 	CalendarRepository calendarRepository;
+	@Autowired
+	ProfileRepository profileRepository;
 	
-	@GetMapping()
-	public List<Calendar> getCalendar(){
-		return calendarRepository.findAll();
+	@GetMapping("{id}")
+	public List<Calendar> getCalendar(@PathVariable Integer id){
+		return calendarRepository.findByProfileId(id);
 	}
 	
-	
 	@PostMapping()
-	public void createCalendar(@RequestBody Calendar calendar) {
-		
-		calendarRepository.save(calendar);
+	public void createCalendar( @RequestBody Calendar calendar) {
+		// should not have to do this but SQLITE is weird
+		Optional<Profile> p = profileRepository.findById(calendar.getProfile().getId());
+		if (p.isPresent()) {
+			calendar.setProfile(p.get());
+			calendarRepository.save(calendar);
+		}
 		
 	}
 
